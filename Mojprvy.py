6dddec02f1c51d8f30bb382897d3b82a3d134938
Michal1408 <michalmucha.skola@gmail.com>
2025-09-17 09:36:54 +0200

    ahoj

--- Mojprvy.py
+++ Mojprvy.py
@@ -1,4 +1,10 @@
-print ("Michal Mucha")
-a = 5
-b = 10
-print (a + b)
+print("Čauko, jednoducho ti viem vypočitať korene hocijakej rovnice. Len budem potrebovať zopar drobnosti")
+a = int(input("A="))
+b = int(input("B="))
+c = int(input("C="))
+print("Diky idem na to")
+import math
+from math import sqrt
+x1 = (-b+sqrt(b**2 - 4 * a * c))/2*a
+x2 = (-b-sqrt(b**2-4*a*c))/2 * a
+print(x1,",",x2)
